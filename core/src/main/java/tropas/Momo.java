9c89ec5bb4d9f6431965d6b0e6af6d73ed2cc227
Leon <leon.rf93@gmail.com>
2024-08-24 20:52:00 -0300

    agregando a momo

---
+++ core/src/main/java/tropas/Momo.java
@@ -0,0 +1,12 @@
+package tropas;
+
+import recursos.Imagen;
+import recursos.Render;
+
+public class Momo extends Tropa {
+	
+	public Momo() {
+		super(new Imagen("img/pjs/ban.jpg", Render.ANCHO, Render.ALTO), 3, 3);
+	}
+
+}
